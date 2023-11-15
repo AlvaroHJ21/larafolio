8f84bfe82c14e72d0ca89822ca41974b7f2cb886
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-11-15 09:57:33 -0500

    update home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-default-layout>
+    <main class="grid place-content-center h-screen container">
+        <p class="text-4xl">👋 Hello, i'm</p>
+        <h1 class="text-6xl md:text-8xl mb-4 uppercase">Alvaro Huaysara Jauregui</h1>
+        <p class="text-4xl mb-8">I'm a Full Stack Web Developer</p>
+        <a href="https://alvarohuaysara.tech/" class="hover:underline hover:text-cyan-700 group">
+            Go to portfolio <i class="fa fa-arrow-right group-hover:translate-x-2 transition-transform"></i>
+        </a>
+    </main>
+</x-default-layout>
